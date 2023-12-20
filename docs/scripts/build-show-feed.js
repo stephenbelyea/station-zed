25f9613565bf5d792942a6309f2a0ab2eee7c91f
Stephen Belyea <belyea.stephen@gmail.com>
2023-12-19 22:32:03 -0500

    Setup filemap for MP3s on Google Drive

--- docs/scripts/build-show-feed.js
+++ docs/scripts/build-show-feed.js
@@ -5,15 +5,25 @@ const showId = episodesElem.getAttribute("data-show-id");
 
 const summaryLength = 250;
 
-const templateEpisode = ({
-  id,
-  imageUrl,
-  title,
-  date,
-  duration,
-  fileUrl,
-  summary,
-}) => `
+let showFiles = [];
+
+const templateEpisodeMp3File = (id) => {
+  const episodeFile = showFiles.find((file) => file.episodeId === id);
+
+  if (!episodeFile)
+    return '<img src="icons/headset.svg" alt="" /> File not available';
+
+  const downloadLink = `https://drive.google.com/u/0/uc?id=${episodeFile.fileId}&export=download`;
+
+  return [
+    `<img src="icons/download.svg" alt="" /> `,
+    `<a href="${downloadLink}" aria-describedby="${id}-title">`,
+    `Download file`,
+    `</a>`,
+  ].join("");
+};
+
+const templateEpisode = ({ id, imageUrl, title, date, duration, summary }) => `
 <article class="episode" id="${id}">
   ${imageUrl ? `<img class="episode-img" src="${imageUrl}" alt="" />` : ""}
   <div class="episode-body">
@@ -23,11 +33,7 @@ const templateEpisode = ({
     <ul class="meta">
       <li><img src="icons/calendar.svg" alt="Release date" /> ${date}</li>
       <li><img src="icons/timer.svg" alt="Episode length" /> ${duration}</li>
-      <li><img src="icons/${fileUrl ? "download" : "headset"}.svg" alt="" /> ${
-  fileUrl
-    ? `<a href="${fileUrl}" aria-describedby="${id}-title">Download file</a>`
-    : "File not available"
-}</li>
+      <li>${templateEpisodeMp3File(id)}</li>
     </ul>
     <p>${
       summary.length > summaryLength
@@ -48,8 +54,21 @@ const getShowFeed = async () => {
   }
 };
 
+const getMp3Filemap = async () => {
+  try {
+    const response = await fetch(`./data/mp3-filemap.json`);
+    return await response.json();
+  } catch (err) {
+    console.log("Error!", err);
+    return null;
+  }
+};
+
 const buildShowFeed = async () => {
   const { episodes } = await getShowFeed();
+  const { files } = await getMp3Filemap();
+  showFiles = files.filter((file) => file.showId === showId);
+
   const episodesHtml = episodes.map(templateEpisode);
   episodesElem.innerHTML = episodesHtml.join("");
 };
